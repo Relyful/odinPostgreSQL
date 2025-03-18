dd6aae471657fb847781a7cd2875ea397b6d78ca
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-18 21:17:21 +0100

    setup index and new get and new post route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const indexRouter = require('./routers/indexRouter');
 const app = express();
 const PORT = 8080;
 
-
-app.get('/', indexRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {console.log(`Server listening on port ${PORT}!`)});
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,15 @@
+const path = require('path');
+
 exports.indexGet = (req, res) => {
   console.log("usernames will be logged here - wip");
   res.send('Hello World!');
 };
+
+exports.newGet = (req, res) => {
+  res.sendFile(path.join(__dirname, '../views', 'newForm.html'));
+};
+
+exports.newPost = (req, res) => {
+  console.log("username to be saved: ", req.body.username);
+  res.redirect("/");
+}
