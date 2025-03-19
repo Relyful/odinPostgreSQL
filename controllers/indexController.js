f6d153a680de80d0c9df7047d3d3943dc862cf51
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-19 21:27:41 +0100

    COnnect to a database and read / create usernames

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,18 @@
 const path = require('path');
+const db = require("../db/queries");
 
-exports.indexGet = (req, res) => {
-  console.log("usernames will be logged here - wip");
-  res.send('Hello World!');
+exports.indexGet = async (req, res) => {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(", "));
 };
 
 exports.newGet = (req, res) => {
   res.sendFile(path.join(__dirname, '../views', 'newForm.html'));
 };
 
-exports.newPost = (req, res) => {
-  console.log("username to be saved: ", req.body.username);
+exports.newPost = async (req, res) => {
+  const { username } = req.body;
+  await db.insertUsername(username);
   res.redirect("/");
 }
